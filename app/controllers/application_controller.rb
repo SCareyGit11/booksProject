b718104c189f62cacfdc49d0003abc6b1a4f7dc8
hkhattar <himanshikhatter@gmail.com>
2016-12-19 14:49:00 -0800

    second user and session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+  	User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def require_login
+    redirect_to '/sessions/new' if session[:user_id] == nil
+  end
+
+  def require_correct_user
+    user = User.find(params[:id])
+    redirect_to "/users/#{current_user.id}" if current_user != user
+  end
+
 end
