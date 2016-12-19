fa7806d5626bdad15a82ba433a7febab3b8278fd
hkhattar <himanshikhatter@gmail.com>
2016-12-19 15:36:53 -0800

    read,current,interested

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,23 @@ class BooksController < ApplicationController
 
   def index
   end
+
+  def create
+  		puts '**********'
+  	puts params[:book][:title]
+  	@book = Book.create(title: params[:book][:title], author: params[:book][:author], category: params[:book][:category], user_id: params[:user_id])
+  	
+  
+  	if params[:book][:shelf] == 'Already read'
+  	@read = Read.create(user_id: session[:user_id],book_id: @book.id)
+  	elsif params[:book][:shelf] == 'Interested in reading'
+  	@interested = Interested.create(user_id: session[:user_id],book_id: @book.id)
+  	elsif params[:book][:shelf] = 'Currently reading'
+  	@current = Current.create(user_id: session[:user_id],book_id: @book.id)
+    end
+
+
+  end
+
+
 end
